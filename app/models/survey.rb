d01ddc793213f7e5c14fe0023a65b2e2f1acf6c7
Niki1720 <xpaula20@wp.pl>
2024-09-14 13:00:15 +0200

    Add validations to model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,14 @@
 class Survey < ApplicationRecord
+  before_save :capitalize_names
+
+  validates :first_name, :last_name, :gender, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :height, presence: true, numericality: { only_integer: true, greater_than: 50, less_than_or_equal_to: 250 }
+
+  private
+
+  def capitalize_names
+    self.first_name = first_name.capitalize
+    self.last_name = last_name.capitalize
+  end
 end
